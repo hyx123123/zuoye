b26eb0ec5ef14190af09905d451fa1cbe9789e48
hyx123123 <149503830+hyx123123@users.noreply.github.com>
2023-11-04 22:06:35 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 int main() {
-	int a[20] = { 0,1,1 };
-	for (int i = 3; i < 20; i++) {
-		a[i] = a[i - 1] + a[i - 2];
+	int a[3][3];
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			scanf_s("%d", &a[i][j]);
+		}
 	}
-	for (int i = 0; i < 20; i++) {
-		printf("%d ", a[i]);
+	int sum1 = 0, sum2 = 0;
+	for (int i = 0; i < 3; i++) {
+		sum1 = sum1 + a[i][i];
 	}
+	for (int i = 0; i < 3; i++) {
+		sum2 = sum2 + a[i][2 - i];
+	}
+	printf("%d", sum1 + sum2);
 	return 0;
 }
-
