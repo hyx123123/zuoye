c7c91ddacb55377b1d26873b471d129cdd1b97b8
hyx123123 <149503830+hyx123123@users.noreply.github.com>
2023-11-04 22:04:30 +0800

    Add files via upload
    3*3

--- 源.c
+++ 源.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
-int main()
-{
-	int n,m;
-	scanf_s("%d%d", &n,&m);
-	int i, j;
-	i = 1;
-	while (i <= n) {
-		j = 1;
-		while (j <= m) {
-			printf("%d*%d=%d", j, i, i * j);
-     		if (i * j < 10) {
-				printf("   ");
-			}
-			else {
-				printf("  ");
-			}
-			j++;
+int main() {
+	int a[3][3];
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			scanf_s("%d", &a[i][j]);
 		}
-		i++;
-		printf("\n");
 	}
+	int sum1 = 0, sum2 = 0;
+	for (int i = 0; i < 3; i++) {
+		sum1 = sum1 + a[i][i];
+	}
+	for (int i = 0; i < 3; i++) {
+		sum2 = sum2 + a[i][2 - i];
+	}
+	printf("%d", sum1 + sum2);
 	return 0;
 }
